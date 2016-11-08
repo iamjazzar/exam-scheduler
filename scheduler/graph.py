dc8022e5eec450eba5c3adfb680ed2c3f539937b
Ahmed Jazzar <me@ahmedjazzar.com>
2016-11-08 23:00:34 +0200

    Sorted nodes in the weight matrix in a descending order

--- scheduler/graph.py
+++ scheduler/graph.py
@@ -1,4 +1,5 @@
-from itertools import permutations
+import itertools
+import operator
 
 from models import Course, Student
 
@@ -19,6 +20,14 @@ class CoursesGraph(object):
 
         self._initialize_nodes()
         self._create_weighted_edges()
+
+        self._color()
+
+    def node_degree(self, node):
+        """
+        :return: a node degree in the graph
+        """
+        return self.graph.degree(node)
 
     def nodes_degree(self):
         """
@@ -46,10 +55,84 @@ class CoursesGraph(object):
             student_courses = student.courses.all()
 
             # permutations of 2 for every course in student's schedule
-            courses_permutations = permutations(student_courses, 2)
+            courses_permutations = \
+                itertools.permutations(student_courses, 2)
 
             for source, dist in courses_permutations:
                 if self.graph.has_edge(source, dist):
                     self.graph[source][dist]['weight'] += 1
                 else:
                     self.graph.add_edge(source, dist, weight=1)
+
+    def _sort_bunch(self, full_list, start, end):
+        """
+        Sorts a bunch of nodes using the weight criteria.
+
+        :param full_list: The list we want to sort its nodes
+        :param start: The start index
+        :param end: The end position (last index + 1)
+
+        :return: Nothing since list object in python are mutable
+        """
+        partial = full_list[start:end]
+
+        sorted_chunk = sorted(
+            self.graph.degree_iter(nbunch=partial, weight='weight'),
+            key=operator.itemgetter(1),
+            reverse=True
+        )
+
+        full_list[start:end] = [c for c, _ in sorted_chunk]
+
+    def _sort_graph(self):
+        """
+        - Sorts the nodes in the weight matrix in a descending order
+          based on the degree of nodes.
+        - Nodes with similar degrees are ordered based on the
+          largest weight w in its adjacency list.
+        - Nodes with similar degrees d and weights w are ordered based
+          on their node ID (smallest ID first).
+
+        :return: A list of sorted nodes
+        """
+
+        # Sort in a descending order based on the degree of the nodes
+        sorted_nodes_degree = sorted(
+            self.graph.degree_iter(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )
+        # The original result will be: (Course, x) while x is the degree
+        # I want to keep the courses only in the list
+        sorted_nodes = [course for course, _ in sorted_nodes_degree]
+
+        # Nodes with similar degrees are ordered based on the largest
+        # weight w in its adjacency list.
+        start = 0
+        similar_degrees = 0
+        for i, (node, degree) in enumerate(sorted_nodes_degree):
+            # If the current degree equals the previous one
+            if i > -1 and degree == sorted_nodes_degree[i-1][1]:
+                # Increment the similar degrees and set the start
+                # index to i-1 if it's the first similar degree
+                similar_degrees += 1
+                if similar_degrees == 1:
+                    start = i-1
+            # If the previous check failed and there are similar degrees
+            elif similar_degrees > 0:
+                # export the end position and reset the similar degrees
+                end = start + similar_degrees + 1
+                similar_degrees = 0
+
+                # And also sort the bunch of founded nodes. No need
+                # to assign a list back because lists in python are
+                # mutable
+                self._sort_bunch(sorted_nodes, start, end)
+
+        return sorted_nodes
+
+    def _color(self):
+        """
+        :return:
+        """
+        self._sort_graph()
